41e2de3a003761a5ed4d158d3e3b47fc16c7b4d6
Ira <zzhanglichuan@outlook.com>
2023-08-06 20:08:58 +0800

    上传导出

---
+++ DlRunCSharp/src/export/ExportHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DlRunCSharp.src.export
+{
+    public class ExportHelper
+    {
+        public const string DllName = "dl-run-tensorrt.dll";
+
+
+        [DllImport(DllName)]
+        public static extern IntPtr himage_to_mat(IntPtr r, IntPtr g, IntPtr b, int height, int width);
+    }
+}
